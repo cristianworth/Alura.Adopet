c39783433f46bb9894ad1439bc4bae311bd0befc
cristianworth <cristianweissmantel@gmail.com>
2024-03-19 19:06:44 -0300

    feat: added static class to convert text to entity pet

--- Alura.Adopet.Console/Util/LeitorDeArquivo.cs
+++ Alura.Adopet.Console/Util/LeitorDeArquivo.cs
@@ -17,14 +17,8 @@ namespace Alura.Adopet.Console.Util
                 System.Console.WriteLine("----- Dados a serem importados -----");
                 while (!sr.EndOfStream)
                 {
-                    // separa linha usando ponto e vírgula
-                    string[]? propriedades = sr.ReadLine().Split(';');
-                    // cria objeto Pet a partir da separação
-                    Pet pet = new Pet(Guid.Parse(propriedades[0]),
-                    propriedades[1],
-                    int.Parse(propriedades[2]) == 1 ? TipoPet.Gato : TipoPet.Cachorro
-                    );
-                    listaDePet.Add(pet);
+                    var linha = sr.ReadLine() ?? "";
+                    listaDePet.Add(linha.ConverteDoTexto());
                 }
             }
 
---
+++ Alura.Adopet.Tests/PetApartirdoCsvTest.cs
@@ -0,0 +1,25 @@
+﻿using Alura.Adopet.Console.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Tests
+{
+    public class PetApartirdoCsvTest
+    {
+        [Fact]
+        public void QuandoStringForValidaDeveRetornarUmPet() 
+        {
+            // Arrange
+            var linha = "456b24f4-19e2-4423-845d-4a80e8854a41;Lima Limão;1";
+
+            // Act
+            var pet = linha.ConverteDoTexto();
+
+            // Assert
+            Assert.NotNull(pet);
+        }
+    }
+}
